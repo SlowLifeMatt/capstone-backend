63e47fbcdde1983cfe7f72203ff2d855692fafb3
Matthew <subinmatt@gmail.com>
2023-03-07 16:49:29 -0800

    Middleware and Model update

--- server.js
+++ server.js
@@ -9,12 +9,28 @@ mongoose.connect(process.env.DATABASE_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   });
+  
+// Middleware
+app.use(cors());
+app.use(morgan("dev")); 
+app.use(express.json()); 
+app.use(express.urlencoded({ extended: false })); 
 
 // Database Connection Logs
 const db = mongoose.connection
 db.on("error", (err) => console.log(err.message))
 db.on("connected", () => console.log("mongo connected"))
 db.on("disconnected", () => console.log("mongo disconnected"))  
+
+// Model
+const TruckSchema = new mongoose.Schema({
+    name: String,
+    image: String,
+    link: String,
+    avgprice: String,
+  });
+
+const Truck = mongoose.model("Truck", TruckSchema)
 
 app.get("/", (req, res) => {
   res.send("Hello World!")
